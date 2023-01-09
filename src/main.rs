d472c3557e94154a5d3ed236b6d7067e7b95b8b9
Andrii Derhach <thequasar7@gmail.com>
2023-01-09 14:18:31 +0200

    Implement color extraction from image

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use clap::{Parser, ValueHint};
+use anyhow::Result;
 use clap::builder::RangedU64ValueParser;
+use clap::{arg, Parser, ValueHint};
+use image::{DynamicImage, GenericImageView};
+use nalgebra_sparse::{CooMatrix, CsrMatrix};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -13,10 +16,25 @@ struct Args {
 
     /// Output file
     #[arg(short = 'o', value_hint = ValueHint::FilePath)]
-    ofile: Option<String>
+    ofile: Option<String>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
-    println!("Hello, world!");
+
+    let colors = extract_colors(&image::open(args.ifile)?);
+    Ok(())
+}
+
+fn extract_colors(image: &DynamicImage) -> Vec<CooMatrix<u8>> {
+    let shape = (image.height() as usize, image.width() as usize);
+    let mut matrices = vec![CooMatrix::<u8>::new(shape.0, shape.1); 4];
+
+    for (col, row, color) in image.pixels() {
+        for (i,each) in color.0.iter().enumerate() {
+            matrices[i].push(row as usize, col as usize, *each);
+        }
+    }
+
+    matrices
 }
