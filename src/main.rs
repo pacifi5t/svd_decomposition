1b23a2627fa851be95227d66179a4677f53bb435
Andrii Derhach <thequasar7@gmail.com>
2023-01-09 08:57:55 +0200

    Add argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{Parser, ValueHint};
+use clap::builder::RangedU64ValueParser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Input file
+    #[arg(value_hint = ValueHint::FilePath)]
+    ifile: String,
+
+    /// Compression level (0 - none, 9 - max)
+    #[arg(short = 'c', value_parser = RangedU64ValueParser::<u8>::new().range(0..=9))]
+    comp: u8,
+
+    /// Output file
+    #[arg(short = 'o', value_hint = ValueHint::FilePath)]
+    ofile: Option<String>
+}
+
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
 }
